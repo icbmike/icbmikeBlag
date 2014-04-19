657aeec3696dfaf2234ab347ffb5c98e2702d8a7
Michael Little <bio595@gmail.com>
2014-04-19 17:46:57 +1200

    started trying to do a repository pattern

---
+++ IcbmikeBlag.Application/Repositories/BlogPostRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IcbmikeBlag.Application.Repositories
+{
+    public class BlogPostRepository : IRepository<int, BlogPost>
+    {
+        public IEnumerable<BlogPost> GetRecentBlogPosts(int numPosts = 10, int page = 1)
+        {
+           
+        }
+
+        public BlogPost GetBlogPost(int postID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveBlogPost(BlogPost post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBlogPost(int postID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
