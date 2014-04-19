b9b6439c290ebf094f27c59e26423df9a21deb51
Michael Little <bio595@gmail.com>
2014-04-19 13:26:34 +1200

    added autofac and DependencyConfig class

---
+++ IcbmikeBlag/App_Start/DependencyConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using IcbmikeBlag.Application.Repositories;
+
+namespace IcbmikeBlag
+{
+    public class DependencyConfig
+    {
+        public static void RegisterDepencies()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<BlogPostRepository>().As<IBlogPostRepository>();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(builder.Build()));
+        }
+    }
+}
