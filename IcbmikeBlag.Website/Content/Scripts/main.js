6cec9f5e693428ef9cc2162edd43eeb888531552
Michael Little <bio595@gmail.com>
2014-04-26 11:28:54 +1200

    Added jquery.debounce for event throttling

--- IcbmikeBlag.Website/Content/Scripts/main.js
+++ IcbmikeBlag.Website/Content/Scripts/main.js
@@ -1,3 +1,10 @@
 ﻿$(function() {
-    
+    //Post Editor
+
+    //Use debounce to throttle previews
+    $(".post-editor .content").on("keyup", $.debounce(function() {
+        var that = this;
+        console.log(that);
+    }, 600));
+
 });
