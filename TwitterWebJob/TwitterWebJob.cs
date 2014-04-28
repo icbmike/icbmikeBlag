2eecd5d5b1cc40b4e96e3fb428f9a2df9715bbc5
Michael Little <bio595@gmail.com>
2014-04-28 19:37:23 +1200

    Added nuget reference to WindowsAzure packages

---
+++ TwitterWebJob/TwitterWebJob.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Jobs;
+
+namespace IcbmikeBlag.TwitterWebJob
+{
+    public class TwitterWebJob
+    {
+        public static void Main(string[] args)
+        {
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        public static void BlogPostQueue([QueueInput("blogPostQueue")]Payload payload)
+        {
+            
+        }
+
+    }
+
+    public class Payload
+    {
+        int PostID { get; set; }
+    }
+}
